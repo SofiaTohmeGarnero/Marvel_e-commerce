4729fc6f5f63b2f38cd53d1ff7da5a35ca80840d
SofiaTohmeGarnero <sofiatohmegarnero@gmail.com>
2022-09-09 01:02:52 -0300

    [FIX] material-theme

--- styles/material-theme.ts
+++ styles/material-theme.ts
@@ -3,6 +3,5 @@ import {createTheme} from "@mui/material";
 export const theme = createTheme({
     typography: {
         fontFamily: 'Roboto, sans-serif',
-        letterSpacing: '.2rem',
     },
 });
